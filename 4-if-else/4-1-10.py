ee11f49b818669599071d21b80c59edba2511020
karlson083 <karlson083@gmail.com>
2022-04-22 00:50:17 +0300

    20220421

---
+++ 4-if-else/4-1-10.py
@@ -0,0 +1,10 @@
+# put your python code here
+v = int(input())
+if v <= 13:
+    print("детство")
+if 14 <= v <= 14:
+    print("молодость")
+if 25 <= v <= 59:
+    print("зрелость")
+if v >= 60:
+    print("старость")
