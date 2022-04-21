fa202f08ddd1e76b50c7638d7d26f1c6d07d2b34
karlson083 <karlson083@gmail.com>
2022-04-21 22:00:30 +0300

    1

---
+++ 2-4-1.py
@@ -0,0 +1,32 @@
+import os
+clear = lambda: os.system('cls')
+clear()
+
+num1 = 7            # num1 - это число
+num2 = 10           # num2 - это число
+num3 = num1 + num2  # num3 - это число
+print(num3)
+
+a = 3
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+num1 = 2 + 3 * 4
+num2 = (2 + 3) * 4
+print(num1)
+print(num2)
+
+s = '1992'
+year = int(s)
+
+"""
+num1 = int(input())
+num2 = int(input())
+print(num1 + num2)
+"""
+
+num = 17
+s = str(17)
