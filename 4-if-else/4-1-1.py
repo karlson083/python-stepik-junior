7dbf239f76a5437caa8871a1a964d983e18ed117
karlson083 <karlson083@gmail.com>
2022-04-22 00:50:27 +0300

    20220421

---
+++ 4-if-else/4-1-1.py
@@ -0,0 +1,43 @@
+import os
+clear = lambda: os.system('cls')
+clear()
+"""
+answer = input('Какой язык программирования мы изучаем?')
+if answer == 'Python':
+    print('Верно! Мы ботаем Python =)')
+    print('Python - отличный язык!')
+"""
+"""
+answer = input('Какой язык программирования мы изучаем?')
+if answer == 'Python':
+    print('Верно! Мы ботаем Python =)')
+    print('Python - отличный язык!')
+else:
+    print('Не совсем так!')
+"""
+
+"""
+num1 = int(input())
+num2 = int(input())
+
+if num1 < num2:
+    print(num1, 'меньше чем', num2)
+if num1 > num2:
+    print(num1, 'больше чем', num2)
+if num1 == num2:                      # используем двойное равенство
+    print(num1, 'равно', num2)
+if num1 != num2:
+    print(num1, 'не равно', num2)
+"""
+
+age = int(input())
+if 3 <= age <= 6:
+    print('Вы ребёнок')
+
+"""
+if a == b == c:
+    print('числа равны')
+else:
+    print('числа не равны')
+
+"""
