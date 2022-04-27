2c75e17906286e1e31222a43e3db11f6a07962bc
karlson083 <karlson083@gmail.com>
2022-04-27 19:37:51 +0300

    ok

---
+++ 4-if-else/4-2-1.py
@@ -0,0 +1,65 @@
+import os
+clear = lambda: os.system('cls')
+clear()
+"""
+age = int(input('Сколько вам лет?: '))
+grade = int(input('В каком классе вы учитесь?: '))
+if age >= 12 and grade >= 7:
+    print('Доступ разрешен.')
+else:
+    print('Доступ запрещен.')
+
+age = int(input('Сколько вам лет?: '))
+grade = int(input('В каком классе вы учитесь?: '))
+city = input('В каком городе вы живете?: ')
+if age >= 12 and grade >= 7 and city == 'Москва':
+    print('Доступ разрешен.')
+else:
+    print('Доступ запрещен.')
+
+    city = input('В каком городе вы живете?: ')
+if city == 'Москва' or city == 'Санкт-Петербург' or city == 'Екатеринбург':
+    print('Доступ разрешен.')
+else:
+    print('Доступ запрещен.')
+
+    age = int(input('Сколько вам лет?: '))
+grade = int(input('В каком классе вы учитесь?: '))
+city = input('В каком городе вы живете?: ')
+if age >= 12 and grade >= 7 and (city == 'Москва' or city == 'Санкт-Петербург'):
+    print('Доступ разрешен.')
+else:
+    print('Доступ запрещен.')
+
+
+age = int(input('Сколько вам лет?: '))
+if not (age < 12):
+    print('Доступ разрешен.')
+else:
+    print('Доступ запрещен.')
+
+#полностью эквивалентен коду:
+
+age = int(input('Сколько вам лет?: '))
+if age >= 12:
+    print('Доступ разрешен.')
+else:
+    print('Доступ запрещен.')
+
+
+num1 = 34
+num2 = 81
+if num1 // 9 == 0 or num2 % 9 == 0:
+    print('число', num1, 'выиграло')
+else:
+    print('число', num2, 'выиграло')
+
+"""
+a = int(input())
+if a >= 2 and a <= 17:
+    b = 3
+    p = a * a + b * b
+else:
+    b = 5
+p = (a + b) * (a + b)
+print(p)
