8bd61eecb29f32799981531dfb861b9c30b61b50
karlson083 <karlson083@gmail.com>
2022-07-30 16:57:07 +0300

    911

--- 9-string/9-1-index/9-1-1about.py
+++ 9-string/9-1-index/9-1-1about.py
@@ -2,3 +2,33 @@ import os
 clear = lambda: os.system('cls')
 clear()
 
+s = 'Hello'
+n = len(s)  # значение переменной равно 5
+print(n)
+
+s = 'All you need is love'
+if 'love' in s:
+    print('❤️')
+else:
+    print('💔')
+
+
+s = 'abcdef'
+for i in range(len(s)):
+    print(s[i])
+
+
+s = 'abcdef'
+for c in s:
+    print(c)
+
+
+s = 'abcdefg'
+print(s[0] + s[2] + s[4] + s[6])
+
+s = 'abcdefg'
+print(s[0]*3 + s[-1]*3 + s[3]*2 + s[3]*2)
+
+s = '01234567891011121314151617'
+for i in range(0, len(s), 5):
+    print(s[i], end='')
