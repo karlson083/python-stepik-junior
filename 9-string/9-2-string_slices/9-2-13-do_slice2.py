8a7a2bc267992bf8ef1c76ac613b50098498abc3
karlson083 <karlson083@gmail.com>
2022-08-03 20:52:35 +0300

    9213

---
+++ 9-string/9-2-string_slices/9-2-13-do_slice2.py
@@ -0,0 +1,32 @@
+import os
+clear = lambda: os.system('cls')
+clear()
+
+#s = input()
+s = "Success is the ability to go from failure to failure without losing your enthusiasm"
+
+print(s[2])
+print(s[-2])
+print(s[:5])
+print(s[:-2])
+print(s[::2])
+print(s[1::2])
+print(s[::-1])
+print(s[::-2])
+
+'''
+Success is the ability to go from failure to failure without losing your enthusia
+Scesi h blt og rmfiuet alr ihu oigyu nhsam
+ucs steaiiyt ofo alr ofiuewtotlsn oretuis
+msaisuhtne ruoy gnisol tuohtiw eruliaf ot eruliaf morf og ot ytiliba eht si sseccuS
+mashn uygio uhi rla teuifmr go tlb h isecS
+
+c
+s
+Succe
+Success is the ability to go from failure to failure without losing your enthusia  
+Scesi h blt og rmfiuet alr ihu oigyu nhsam
+ucs steaiiyt ofo alr ofiuewtotlsn oretuis
+msaisuhtne ruoy gnisol tuohtiw eruliaf ot eruliaf morf og ot ytiliba eht si sseccuS
+mashn uygio uhi rla teuifmr go tlb h isec
+'''
