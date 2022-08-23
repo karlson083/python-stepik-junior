47410d6eca4338f1b8a2a139dbaf9ef74d8d5491
karlson083 <karlson083@gmail.com>
2022-08-23 20:40:15 +0300

    950-all

--- 9-string/9-5-string_methods3/9-5-1-about.py
+++ 9-string/9-5-string_methods3/9-5-1-about.py
@@ -2,3 +2,17 @@ from audioop import mul
 import os
 clear = lambda: os.system('cls')
 clear()
+
+s = 'aabbAA111ccDDaa'
+print(s.isalnum())
+print(s.isalpha())
+print(s.isdigit())
+
+s = 'aabb!@#$11cc'
+print(s.islower())
+
+s = 'AAb!@#$11CC'
+print(s.isupper())
+
+s = '    abbc    '
+print(s.isspace())
---
+++ 9-string/9-5-string_methods3/9-5-8.py
@@ -0,0 +1,12 @@
+from audioop import mul
+import os
+clear = lambda: os.system('cls')
+clear()
+
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
+
+#In 2010, someone paid 10K Bitcoin for two pizzas.
