f390904db5020579dec630e8ec9619968ee2af3c
karlson083 <karlson083@gmail.com>
2022-09-11 15:08:42 +0300

    11-7-all-ok

--- 11-list/11-7-list_example/11-7-10.py
+++ 11-list/11-7-list_example/11-7-10.py
@@ -12,17 +12,4 @@ for i in l:
         print(int(i) ** 2, end = ' ')
   '''  
 
-
-
 print(*[int(i) ** 2 for i in input().split() if int(i) % 2 == 0 and int(i) ** 2 % 10 != 4])
-
-
-
-'''
-print(*[i for i in input() if i in ['1','2','3','4','5','6','7','8','9','0']], sep = '')
-
-
-
-print(*(i for i in input() if i.isdigit()), sep="")
-
-'''
