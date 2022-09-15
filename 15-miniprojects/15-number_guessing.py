4505e38a2f68a61f0e45673aee4e50e02f3829c5
karlson083 <karlson083@gmail.com>
2022-09-15 17:33:42 +0300

    add max gen digital

--- 15-miniprojects/15-number_guessing.py
+++ 15-miniprojects/15-number_guessing.py
@@ -4,28 +4,41 @@ from posixpath import split
 clear = lambda: os.system('cls')
 clear()
 
-rndx = random.randint(1,100) # Задаем рандомом целочисленное значение которое нужно угадать
+
 print('Добро пожаловать в числовую угадайку')
-#print(rndx)
+
+def rnd_gen():
+    max_gen_in = int(input('Введите целое число больше 1 для задания верхнего предела генерации числа '))
+    while max_gen_in < 1:
+        print('Вы ввеели число меньше или равное 1')
+        max_gen_in = int(input('Введите целое число больше 1 для задания верхнего предела генерации числа '))
+    rndx_in = random.randint(1,max_gen_in) # Задаем рандомом целочисленное значение которое нужно угадать
+    print('Новое число которе нужно угадать сгенерировано в диапазоне от 1 до ', max_gen_in)
+    print('Начинаем угадывать =)')
+    return rndx_in , max_gen_in
+
+rndx, max_gen = rnd_gen()
+
 # Проверка на корретность введенного значения для угадывания
-def is_valid(check_x):
-    return check_x.isdigit() and 1 <= int(check_x) <= 100 
+def is_valid(check_x,max_gen_in):
+    return check_x.isdigit() and 1 <= int(check_x) <= max_gen_in 
 
 #Ввод числа для угазывания пользователем
-def input_x():
-    return input('Введите число от 1 до 100 - ')
+def input_x(max_gen_in):
+    print('Введите число от 1 до', max_gen_in, '- ', end = '')
+    return input()
 
 #Основной цикл проверки правильности ввода
-def general_input():
-    x = input_x()
-    while not is_valid(x):
-        print('А может быть все-таки введем целое число от 1 до 100?')
-        x = input_x()
+def general_input(max_gen_in):
+    x = input_x(max_gen_in)
+    while not is_valid(x,max_gen_in):
+        print('А может быть все-таки введем целое число от 1 до', max_gen_in,'?')
+        x = input_x(max_gen_in)
     return int(x)
 
 count_try = 0
 while 1:
-    i = general_input() 
+    i = general_input(max_gen) 
     count_try += 1
     if i < rndx:
         print('Ваше число меньше загаданного, попробуйте еще разок')
@@ -38,8 +51,9 @@ while 1:
             print('Вы ввели неверно введите да или нет')
             answer = input('Хотите сыграть еще раз? да, нет.')
         if answer == 'да':
+            clear()
             count_try = 0
-            rndx = random.randint(1,100)
+            rndx, max_gen = rnd_gen()
         elif answer == 'нет':
             break
 print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
