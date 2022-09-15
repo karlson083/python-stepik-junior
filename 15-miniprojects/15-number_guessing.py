8581dc3493281873dfa8ef78b859d89d475be2b7
karlson083 <karlson083@gmail.com>
2022-09-15 16:31:34 +0300

    AllWorkOK

--- 15-miniprojects/15-number_guessing.py
+++ 15-miniprojects/15-number_guessing.py
@@ -4,20 +4,37 @@ from posixpath import split
 clear = lambda: os.system('cls')
 clear()
 
-
-
-rndx = random.randint(1,100)
+rndx = random.randint(1,100) # Задаем рандомом целочисленное значение которое нужно угадать
 print('Добро пожаловать в числовую угадайку')
-
+print(rndx)
+# Проверка на корретность введенного значения для угадывания
 def is_valid(check_x):
     return check_x.isdigit() and 1 <= int(check_x) <= 100 
 
+#Ввод числа для угазывания пользователем
 def input_x():
     return input('Введите число от 1 до 100 - ')
 
-x = input_x()
-
-while not is_valid(x):
-    print('А может быть все-таки введем целое число от 1 до 100?')
+#Основной цикл проверки правильности ввода
+def general_input():
     x = input_x()
+    while not is_valid(x):
+        print('А может быть все-таки введем целое число от 1 до 100?')
+        x = input_x()
+    return int(x)
+
+while 1:
+    i = general_input() 
+    if i < rndx:
+        print('Ваше число меньше загаданного, попробуйте еще разок')
+    elif i > rndx:
+        print('Ваше число больше загаданного, попробуйте еще разок')
+    elif i == rndx:
+        print('Вы угадали, поздравляем!')
+        break
+print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+
+
+
+
 
