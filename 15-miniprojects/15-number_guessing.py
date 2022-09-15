ceca6d004b7da0585b4cc6533d1c8d816c71ae3c
karlson083 <karlson083@gmail.com>
2022-09-15 16:41:46 +0300

    added count tryed

--- 15-miniprojects/15-number_guessing.py
+++ 15-miniprojects/15-number_guessing.py
@@ -6,7 +6,7 @@ clear()
 
 rndx = random.randint(1,100) # Задаем рандомом целочисленное значение которое нужно угадать
 print('Добро пожаловать в числовую угадайку')
-print(rndx)
+#print(rndx)
 # Проверка на корретность введенного значения для угадывания
 def is_valid(check_x):
     return check_x.isdigit() and 1 <= int(check_x) <= 100 
@@ -23,14 +23,16 @@ def general_input():
         x = input_x()
     return int(x)
 
+count_try = 0
 while 1:
     i = general_input() 
+    count_try += 1
     if i < rndx:
         print('Ваше число меньше загаданного, попробуйте еще разок')
     elif i > rndx:
         print('Ваше число больше загаданного, попробуйте еще разок')
     elif i == rndx:
-        print('Вы угадали, поздравляем!')
+        print('Вы угадали, за',count_try,'попыток поздравляем!')
         break
 print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
 
