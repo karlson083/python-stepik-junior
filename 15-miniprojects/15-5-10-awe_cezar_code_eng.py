16ca878dea53ff3b925636c66675236187803de4
karlson083 <karlson083@gmail.com>
2022-12-20 20:36:33 +0300

    202212202036

---
+++ 15-miniprojects/15-5-10-awe_cezar_code_eng.py
@@ -0,0 +1,46 @@
+
+import os
+clear = lambda: os.system('cls')
+clear()
+
+text = 'Sgd fqzrr hr zkvzxr fqddmdq nm sgd nsgdq rhcd ne sgd edmbd.'
+#diff = -25
+text_list = text.split()
+
+for i in text_list:
+    print(i,len(i))
+print(text_list) 
+def decode(text,diff):
+    if diff > 0:
+        for i in text:
+            if 65 <= ord(i) <= 91:
+                print(chr(ord(i) + diff - 26) if ord(i) + diff > 91 else (chr(ord(i) + diff) if 65 <= ord(i) <= 91 else i), end = '')
+            elif 97 <= ord(i) <= 122:
+                print(chr(ord(i) + diff - 26) if ord(i) + diff > 122 else (chr(ord(i) + diff) if 97 <= ord(i) <= 122 else i), end = '')
+            else:
+                print(i, end = '')
+    elif diff < 0:
+        for i in text:
+            if 65 <= ord(i) <= 91:
+                print(chr(ord(i) + diff + 26) if ord(i) + diff < 65 else (chr(ord(i) + diff) if 65 <= ord(i) <= 91 else i), end = '')
+            elif 97 <= ord(i) <= 122:
+                print(chr(ord(i) + diff + 26) if ord(i) + diff < 97 else (chr(ord(i) + diff) if 97 <= ord(i) <= 122 else i), end = '')
+            else:
+                print(i, end = '')
+    else:
+        print(text)
+pass
+
+for i in range(0,-26,-1):
+    print(i, end = ' ')
+    decode('Hawnj pk swhg xabkna ukq nqj.',i)
+    print()
+
+
+
+
+'''
+for i in range(65,123):
+    print(i, chr(i), end = ' ')
+    
+'''
