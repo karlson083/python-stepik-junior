de2858400c3b8ec2a4f79e086f24cd22e91df209
karlson083 <karlson083@gmail.com>
2022-12-23 18:53:44 +0300

    20221222

--- 15-miniprojects/15-7.py
+++ 15-miniprojects/15-7.py
@@ -3,8 +3,108 @@ import os,random
 clear = lambda: os.system('cls')
 clear()
 
-word_list = ["вася", "петя"]
+word_list = ["вася", "петя",'ктото','колия']
+
+def input_chairs():
+    str0 = input('Введите слово или букву')
+    return str0
+
+def get_word(wd_list): # функция получения рандомного слова
+    return wd_list[random.randint(0,len(wd_list)-1)].upper()
+
+def display_hangman(tries): # функция получения текущего состояния
+    stages = [  # финальное состояние: голова, торс, обе руки, обе ноги
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                ''',
+                # голова, торс, обе руки, одна нога
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                ''',
+                # голова, торс, обе руки
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                ''',
+                # голова, торс и одна рука
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                ''',
+                # голова и торс
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                ''',
+                # голова
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                ''',
+                # начальное состояние
+                '''
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                '''
+    ]
+    return stages[tries]
+
+def play(word):
+    # тело функции
+    word_completion = '_' * len(word)  # строка, содержащая символы _ на каждую букву задуманного слова
+    guessed = False                    # сигнальная метка
+    guessed_letters = []               # список уже названных букв
+    guessed_words = []                 # список уже названных слов
+    tries = 6                          # количество попыток
+    print('Lets play')
+    print(display_hangman(6),' Осталось промахов - ',tries)
+    print(word_completion)
+    str1 = input_chairs()
+    if len(str1) != 1:
+        print(str1)
+
+    pass
 
 
-def get_word(wd_list):
-    
+"""
+for _ in range(1,random.randint(1,100)):
+    print(get_word(word_list))
+    print(display_hangman(random.randint(0,6)))
+"""
